b69eba79e5fcea2bb7bc99214ab42647925b0924
Susmit <35606153+susmitkalyan@users.noreply.github.com>
2023-10-13 01:02:31 +0530

    Update Sum of 5 Digit Number.c

--- Sum of 5 Digit Number/Sum of 5 Digit Number.c
+++ Sum of 5 Digit Number/Sum of 5 Digit Number.c
@@ -2,7 +2,7 @@
 int main()
 {
 	int i,d1,d2,d3,d4,d5,f1,f2,f3,r;
-	printf("Input a 5 digit number\n");
+	printf("Input a 5 digit number: ");
 	scanf("%d", &i);
 	d1=i/10000;
 	f1=i%10000;
@@ -13,6 +13,5 @@ int main()
 	d4=f3/10;
 	d5=f3%10;
 	r=d1+d2+d3+d4+d5;
-	printf("The result is %d",r);
-	getch();
+	printf("The result is %d",r);
 }
