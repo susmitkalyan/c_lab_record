5224865e6b08172e8f7d0cd9e640a8c45c75ce48
Susmit <35606153+susmitkalyan@users.noreply.github.com>
2023-11-16 02:22:52 +0530

    Update alternate_case.c

--- alternate_case.c
+++ alternate_case.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Input a Character: ");
+    ch=getchar();
+    if(ch>=65&&ch<=90)
+    {
+        ch=ch+32;
+    }
+    else
+    {
+        ch=ch-32;
+    }
+    printf("Result: %c",ch);
+}
